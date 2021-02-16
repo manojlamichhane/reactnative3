6242aea6695cbd83cd297c41bf281cb79c151b17
manojlamichhane <lamichhanemanoj1@gmail.com>
2021-02-16 19:28:48 +0545

    textinputform

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View, Alert, Button } from "react-native";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const formHandler = () => {
+    Alert.alert(
+      "Do you want to continue?",
+      "You are about to login to this page",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Login canceled"),
+        },
+        {
+          text: "OK",
+          onPress: () => console.log("Login successful"),
+        },
+      ]
+    );
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.Inputfield}
+        placeholder="Username"
+        value={username}
+        onChangeText={(e) => setUsername(e)}
+      />
+      <TextInput
+        style={styles.Inputfield}
+        placeholder="Password"
+        value={password}
+        onChangeText={(e) => setPassword(e)}
+      />
+      <Button title="Login" onPress={formHandler} />
     </View>
   );
 }
@@ -14,8 +43,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  Inputfield: {
+    paddingLeft: 20,
+    width: 300,
+    height: 40,
+    borderRadius: 10,
+    borderColor: "grey",
+    borderWidth: 1,
+    backgroundColor: "white",
   },
 });
